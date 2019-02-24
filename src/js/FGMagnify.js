250a9cfda3fa62f55c823ac32d34b4e492e33e71
Mark Mayes <git@firmgently.co.uk>
2019-02-24 11:06:09 +0000

    updated item page layout, tweaked mag to work with new centred images

--- src/js/FGMagnify.js
+++ src/js/FGMagnify.js
@@ -11,7 +11,7 @@ uk.co.firmgently.FGMagnify = (function () {
   // constants
   var
   FGMAG_RFR_MS = 100, MAGSHOW_DELAY_MS = 600, 
-  UPDATE_DELAY_MS = 100,
+  UPDATE_DELAY_MS = 50,
   ZOOMED_IMAGE_WIDTH = 3000,
   IMAGESIZE_TOPMARGIN_NORMAL = 200, IMAGESIZE_TOPMARGIN_SMALL = 70,
   IMAGESIZE_LEFTMARGIN_SMALL = 70, IMAGESIZE_LEFTMARGIN_NORMAL = 100,
@@ -23,7 +23,7 @@ uk.co.firmgently.FGMagnify = (function () {
   useLightColours,
   updateMag_tmr, magShow_tmr, onScroll_tmr, onResize_tmr,
   loupeBnd_rct = {}, mainImg_rct = {},
-  hiResImg_inf = {}, colours_inf = {}, size_inf = {}, mouse_inf = {},
+  hiResImg_info = {}, colours_info = {}, size_info = {}, mouse_info = {},
   loupeBgResize, loupeBgImgOrig,
   itemDetails_el, imageMainContainer_el, mainColumn_el,
   magIsShowing,
@@ -115,14 +115,14 @@ uk.co.firmgently.FGMagnify = (function () {
   
   updateMag = function() { 
     var
-      x = clamp(mouse_inf.xMainImg, loupeBnd_rct.left, loupeBnd_rct.right),
-      y = clamp(mouse_inf.yMainImg, loupeBnd_rct.top, loupeBnd_rct.bottom);
+      x = clamp(mouse_info.xMainImg, loupeBnd_rct.left, loupeBnd_rct.right),
+      y = clamp(mouse_info.yMainImg, loupeBnd_rct.top, loupeBnd_rct.bottom);
     // set loupe position
-    loupe.style.left = Math.round(x - size_inf.loupeHW - size_inf.loupeBorder) + "px";
-    loupe.style.top = Math.round(y - size_inf.loupeHH - size_inf.loupeBorder) + "px";
+    loupe.style.left = mainImg.offsetLeft + Math.round(x - size_info.loupeHW - size_info.loupeBorder) + "px";
+    loupe.style.top = Math.round(y - size_info.loupeHH - size_info.loupeBorder) + "px";
     // set mag image position
-    hiResImg.style.left = Math.round(size_inf.magHW - ((x - size_inf.mainImgBorder) * size_inf.ratio)) + "px";
-    hiResImg.style.top = Math.round(size_inf.magHH - ((y - size_inf.mainImgBorder) * size_inf.ratio)) + "px";
+    hiResImg.style.left = Math.round(size_info.magHW - ((x - size_info.mainImgBorder) * size_info.ratio)) + "px";
+    hiResImg.style.top = Math.round(size_info.magHH - ((y - size_info.mainImgBorder) * size_info.ratio)) + "px";
     
     // recur
     updateMag_tmr = setTimeout(updateMag, FGMAG_RFR_MS);
@@ -135,7 +135,7 @@ uk.co.firmgently.FGMagnify = (function () {
   */
 
   showMag = function() {
-    if ( ! hiResImg_inf.loadStarted) { loadHiResImage(); }
+    if ( ! hiResImg_info.loadStarted) { loadHiResImage(); }
     hiResImg.style.visibility = "visible";
     mag.style.display = "inline-block";
     loupe.style.display = "inline-block";
@@ -160,14 +160,14 @@ uk.co.firmgently.FGMagnify = (function () {
     // rather than having to wait for the hi-res to load
     hiResImg.setAttribute("src", mainImg.src);
     addClassname(hiResImg, "loading");
-    hiResImg_inf.loadStarted = true;
+    hiResImg_info.loadStarted = true;
     hiResImg.width = ZOOMED_IMAGE_WIDTH;
     calculateSizes();
     updateMag();
 
     hiResImgPreload_el = document.createElement("img");
     registerEventHandler(hiResImgPreload_el, "load", onHiResImageLoad);
-    hiResImgPreload_el.setAttribute("src", hiResImg_inf.path);
+    hiResImgPreload_el.setAttribute("src", hiResImg_info.path);
 
     updateMag_tmr = setTimeout(updateMag, FGMAG_RFR_MS);
   };
@@ -181,10 +181,10 @@ uk.co.firmgently.FGMagnify = (function () {
 
   
   updateMousePos = function(e) {
-    mouse_inf.x = e.touches ? e.touches[0].clientX : e.clientX;
-    mouse_inf.y = e.touches ? e.touches[0].clientY : e.clientY;
-    mouse_inf.xMainImg = mouse_inf.x - size_inf.mainImgRealL;
-    mouse_inf.yMainImg = mouse_inf.y - size_inf.mainImgRealT + getWindowScrollPos();
+    mouse_info.x = e.touches ? e.touches[0].clientX : e.clientX;
+    mouse_info.y = e.touches ? e.touches[0].clientY : e.clientY;
+    mouse_info.xMainImg = mouse_info.x - size_info.mainImgRealL;
+    mouse_info.yMainImg = mouse_info.y - size_info.mainImgRealT + getWindowScrollPos();
   };
 
   
@@ -198,22 +198,22 @@ uk.co.firmgently.FGMagnify = (function () {
         
     // which side has most room?
     // check top side, call it biggest
-    biggestMeasurement = size_inf.mainImgRealT - scrollPos;
+    biggestMeasurement = size_info.mainImgRealT - scrollPos;
     biggestSide = "top";
     // check right side - bigger than biggest?
-    curMeasurement = winSize_ar[0] - (size_inf.mainImgRealL + mainImg.offsetWidth);
+    curMeasurement = winSize_ar[0] - (size_info.mainImgRealL + mainImg.offsetWidth);
     if (curMeasurement > biggestMeasurement) {
       biggestMeasurement = curMeasurement;
       biggestSide = "right";
     }
     // check bottom side - bigger than biggest?
-    curMeasurement = winSize_ar[1] - (size_inf.mainImgRealT + mainImg.offsetHeight) + scrollPos;
+    curMeasurement = winSize_ar[1] - (size_info.mainImgRealT + mainImg.offsetHeight) + scrollPos;
     if (curMeasurement > biggestMeasurement) {
       biggestMeasurement = curMeasurement;
       biggestSide = "bottom";
     }
     // check left side, bigger than biggest?
-    curMeasurement = size_inf.mainImgRealL;
+    curMeasurement = size_info.mainImgRealL;
     if (curMeasurement > biggestMeasurement) {
       biggestMeasurement = curMeasurement;
       biggestSide = "left";
@@ -221,60 +221,60 @@ uk.co.firmgently.FGMagnify = (function () {
 
     switch (biggestSide) {
       case "top":
-        size_inf.magW = mainImg.offsetWidth;
-        size_inf.magH = biggestMeasurement;
-        mag.style.left = size_inf.mainImgRealL + "px";
+        size_info.magW = mainImg.offsetWidth;
+        size_info.magH = biggestMeasurement;
+        mag.style.left = size_info.mainImgRealL + "px";
         mag.style.top = scrollPos + "px";
         break;
       case "right":
-        size_inf.magW = biggestMeasurement - PAGE_MARGIN - (size_inf.magBorder * 2);
-        size_inf.magH = mainImg.offsetHeight;
-        mag.style.left = (size_inf.mainImgRealL + mainImg.offsetWidth) + "px";
-        mag.style.top = size_inf.mainImgRealT + "px";
+        size_info.magW = biggestMeasurement - PAGE_MARGIN - (size_info.magBorder * 2);
+        size_info.magH = mainImg.offsetHeight;
+        mag.style.left = (size_info.mainImgRealL + mainImg.offsetWidth) + "px";
+        mag.style.top = size_info.mainImgRealT + "px";
         break;
       case "bottom":
-        size_inf.magW = mainImg.offsetWidth;
-        size_inf.magH = biggestMeasurement;
-        mag.style.left = size_inf.mainImgRealL + "px";
-        mag.style.top = (size_inf.mainImgRealT + mainImg.offsetHeight) + "px";
+        size_info.magW = mainImg.offsetWidth;
+        size_info.magH = biggestMeasurement;
+        mag.style.left = size_info.mainImgRealL + "px";
+        mag.style.top = (size_info.mainImgRealT + mainImg.offsetHeight) + "px";
         break;
       case "left":
-        size_inf.magW = biggestMeasurement;
-        size_inf.magH = mainImg.offsetHeight;
+        size_info.magW = biggestMeasurement;
+        size_info.magH = mainImg.offsetHeight;
         mag.style.left = 0 + "px";
-        mag.style.top = size_inf.mainImgRealT + "px";
+        mag.style.top = size_info.mainImgRealT + "px";
         break;
     }
 
     // take general measurements of magnifier elements
-    mag.style.width = size_inf.magW + "px";
-    mag.style.height = size_inf.magH + "px"; 
-    magInner.style.width = size_inf.magW + "px";
-    magInner.style.height = size_inf.magH + "px";
-    // work out size_inf.ratio of magnified:small images
-    size_inf.ratio = hiResImg.width / (mainImg.width + size_inf.loupeBorder * 2);
-    if (size_inf.ratio === 0) {size_inf.ratio = 1;}
+    mag.style.width = size_info.magW + "px";
+    mag.style.height = size_info.magH + "px"; 
+    magInner.style.width = size_info.magW + "px";
+    magInner.style.height = size_info.magH + "px";
+    // work out size_info.ratio of magnified:small images
+    size_info.ratio = hiResImg.width / (mainImg.width + size_info.loupeBorder * 2);
+    if (size_info.ratio === 0) {size_info.ratio = 1;}
     // size loupe accordingly
-    size_inf.loupeW = size_inf.magW / size_inf.ratio;
-    size_inf.loupeH = size_inf.magH / size_inf.ratio;
-    loupe.style.width = size_inf.loupeW + "px";
-    loupe.style.height = size_inf.loupeH + "px";
+    size_info.loupeW = size_info.magW / size_info.ratio;
+    size_info.loupeH = size_info.magH / size_info.ratio;
+    loupe.style.width = size_info.loupeW + "px";
+    loupe.style.height = size_info.loupeH + "px";
     // measure some more important elements
-    size_inf.hiResHW = hiResImg.width / 2;
-    size_inf.hiResHH = hiResImg.height / 2;
-    size_inf.loupeHW = size_inf.loupeW / 2;
-    size_inf.loupeHH = size_inf.loupeH / 2;
-    size_inf.magHW = size_inf.magW / 2;
-    size_inf.magHH = size_inf.magH / 2;
+    size_info.hiResHW = hiResImg.width / 2;
+    size_info.hiResHH = hiResImg.height / 2;
+    size_info.loupeHW = size_info.loupeW / 2;
+    size_info.loupeHH = size_info.loupeH / 2;
+    size_info.magHW = size_info.magW / 2;
+    size_info.magHH = size_info.magH / 2;
     // set up boundaries
-    loupeBnd_rct.left = size_inf.loupeHW + size_inf.loupeBorder + size_inf.mainImgBorder;
-    loupeBnd_rct.right = (mainImg.offsetWidth - size_inf.loupeHW) + (size_inf.loupeBorder * 2) + (size_inf.mainImgBorder);
-    loupeBnd_rct.top = size_inf.loupeHH + size_inf.loupeBorder + (size_inf.loupeBorder) + (size_inf.mainImgBorder);
-    loupeBnd_rct.bottom = (mainImg.offsetHeight - size_inf.loupeHH) + size_inf.loupeBorder;
-    mainImg_rct.left = mainImg.offsetLeft + size_inf.magHW;
-    mainImg_rct.right = mainImg.offsetLeft + mainImg.offsetWidth - size_inf.magHW;
-    mainImg_rct.top = mainImg.offsetTop + size_inf.magHH;
-    mainImg_rct.bottom = mainImg.offsetTop + mainImg.offsetHeight - size_inf.magHH;
+    loupeBnd_rct.left = size_info.loupeHW + size_info.loupeBorder + size_info.mainImgBorder;
+    loupeBnd_rct.right = (mainImg.offsetWidth - size_info.loupeHW) + (size_info.loupeBorder * 2) + (size_info.mainImgBorder);
+    loupeBnd_rct.top = size_info.loupeHH + size_info.loupeBorder + (size_info.loupeBorder) + (size_info.mainImgBorder);
+    loupeBnd_rct.bottom = (mainImg.offsetHeight - size_info.loupeHH) + (size_info.loupeBorder * 2) + size_info.mainImgBorder;
+    mainImg_rct.left = mainImg.offsetLeft + size_info.magHW;
+    mainImg_rct.right = mainImg.offsetLeft + mainImg.offsetWidth - size_info.magHW;
+    mainImg_rct.top = mainImg.offsetTop + size_info.magHH;
+    mainImg_rct.bottom = mainImg.offsetTop + mainImg.offsetHeight - size_info.magHH;
   };
   
   
@@ -283,19 +283,19 @@ uk.co.firmgently.FGMagnify = (function () {
 
     // set light/dark
     if(useLightColours) {
-      colours_inf.borderLimit = BORDERCOLOUR_LIMIT_LIGHT;
+      colours_info.borderLimit = BORDERCOLOUR_LIMIT_LIGHT;
       loupeBgResize = LOUPEBG_RESIZE_LIGHT;
-      colours_inf.attract = BORDERCOLOUR_LIMIT_LIGHT;
+      colours_info.attract = BORDERCOLOUR_LIMIT_LIGHT;
     } else {
-      colours_inf.borderLimit = BORDERCOLOUR_LIMIT_DARK;
+      colours_info.borderLimit = BORDERCOLOUR_LIMIT_DARK;
       loupeBgResize = LOUPEBG_RESIZE_DARK;
-      colours_inf.attract = BORDERCOLOUR_LIMIT_DARK;
+      colours_info.attract = BORDERCOLOUR_LIMIT_DARK;
     }
 
     // get parent
     parentElement = mainImg.parentNode;
-    size_inf.parentL = parentElement.offsetLeft;
-    size_inf.parentT = parentElement.offsetTop;
+    size_info.parentL = parentElement.offsetLeft;
+    size_info.parentT = parentElement.offsetTop;
     
     // make loupe and attach it to inner container
     loupe = document.createElement("div");
@@ -304,8 +304,8 @@ uk.co.firmgently.FGMagnify = (function () {
     loupe.style.pointerEvents = "none";
     loupeBgImgOrig = loupe.style.backgroundImage;
     // ! style measurements must be taken whilst object is displayed on page !
-    size_inf.loupeBorder = (loupe.offsetWidth - loupe.clientWidth) / 2;
-    loupe.style.margin = "-" + size_inf.loupeBorder + "px 0 0 -" + size_inf.loupeBorder + "px";
+    size_info.loupeBorder = (loupe.offsetWidth - loupe.clientWidth) / 2;
+    loupe.style.margin = "-" + size_info.loupeBorder + "px 0 0 -" + size_info.loupeBorder + "px";
     loupeBnd_rct = {};
     
     // make outer container for hi-res image and attach it to DOM
@@ -313,8 +313,8 @@ uk.co.firmgently.FGMagnify = (function () {
     mag.id = "mag";
     document.body.appendChild(mag);
     // set position of magnifier (first frame of entry animation)
-    size_inf.magBorder = (mag.offsetWidth - mag.clientWidth) / 2;
-    colours_inf.borderOrig = mag.style.borderColor;
+    size_info.magBorder = (mag.offsetWidth - mag.clientWidth) / 2;
+    colours_info.borderOrig = mag.style.borderColor;
     
     // create image loading message elements
     imgLoadingMsg = document.createElement("p");
@@ -344,12 +344,12 @@ uk.co.firmgently.FGMagnify = (function () {
   updateMainImageMetrics = function() {
     // measure border and position of main image
     var curElement = mainImg;
-    size_inf.mainImgBorder = Math.round((mainImg.offsetWidth - mainImg.clientWidth) / 2);
-    size_inf.mainImgRealL = 0;
-    size_inf.mainImgRealT = 0;
+    size_info.mainImgBorder = Math.round((mainImg.offsetWidth - mainImg.clientWidth) / 2);
+    size_info.mainImgRealL = 0;
+    size_info.mainImgRealT = 0;
     while(curElement.tagName !== "HTML" && curElement.tagName !== "BODY") {
-      size_inf.mainImgRealT += curElement.offsetTop;
-      size_inf.mainImgRealL += curElement.offsetLeft;
+      size_info.mainImgRealT += curElement.offsetTop;
+      size_info.mainImgRealL += curElement.offsetLeft;
       curElement = curElement.offsetParent;
     }
   };
@@ -450,7 +450,7 @@ uk.co.firmgently.FGMagnify = (function () {
     */
     create: function(hiResPath, image_ob, useLightColours) { // create our magnifier
       mainImg = document.getElementById(image_ob);
-      hiResImg_inf.path = hiResPath;
+      hiResImg_info.path = hiResPath;
       useLightColours = useLightColours;
       if (mainImg) {
         addClassname(mainImg, "invisible");
